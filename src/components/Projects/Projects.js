5dce8e86fb0f385a517ec3bb7cf54ef686c2fec3
Md. Firoze Hossain <firoze.hossain03@outlook.com>
2023-09-08 21:18:31 +0600

    project list updated updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,9 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import codeChef from "../../Assets/Projects/codeChef.jpg";
-import codeForces from "../../Assets/Projects/codeforces.png";
-import leetCode from "../../Assets/Projects/leetCode.png";
+import ilife from "../../Assets/Projects/i-life.jpg";
+import geninsurance from "../../Assets/Projects/geninsurance.png";
 
 
 function Projects() {
@@ -21,19 +20,19 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
         <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leetCode}
+              imgPath={ilife}
               isBlog={false}
-              title="LeetCode"
-              description=""
-              link="https://leetcode.com/firoze_hossain/"
+              title="i-life"
+              description="i-Life is an internet based life insurance system. This is a responsive application. So, this system accessible through Smart Phone, Tablet, Laptop and desktop."
+              link="https://leads.com.bd/insurance-solutions/internet-insurance-solution/"
             />
           </Col>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={codeChef}
+              imgPath={geninsurance}
               isBlog={false}
-              title="CodeChef"
-              description=""
+              title="General Insurance"
+              description="A comprehensive software solution for general insurance can efficiently manage motor, marine, and fire insurance policies. It offers a user-friendly interface for policyholders to purchase, renew, and make claims seamlessly. The system incorporates robust risk assessment algorithms, enabling accurate premium calculations based on factors like vehicle type, cargo value, and property specifications. Additionally, it streamlines claims processing, ensuring timely settlements for accidents, maritime incidents, or fire-related losses, enhancing overall customer satisfaction and operational efficiency."
               link="https://www.codechef.com/users/firoze_hossain"
             />
           </Col>
