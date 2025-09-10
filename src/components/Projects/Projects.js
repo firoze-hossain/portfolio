cd9f7ab89055b7fbed16e7bf5a3b0cdce2bb27ed
firoze-hossain <firoze.hossain@outlook.com>
2025-09-10 17:18:28 +0600

    new Project section updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,8 +3,8 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards"; // assuming your ProjectCard component already exists
 import Particle from "../Particle";
 import hos from "../../Assets/Projects/hos.jpeg";
-import geninsurance from "../../Assets/Projects/geninsurance.png";
-import smarthr from "../../Assets/Projects/smarthr.png"; 
+import smarthr from "../../Assets/Projects/smarthr.png";
+import thundercall from "../../Assets/Projects/Thundercall.png";
 
 function Projects() {
   return (
@@ -27,7 +27,15 @@ function Projects() {
               link="https://github.com/firoze-hossain/SmartHR"
             />
           </Col>
-
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={thundercall}
+              isBlog={false}
+              title="Thundercall"
+              description="Thundercall is a powerful API testing tool that strikes with the speed and force of lightning, allowing you to effortlessly debug and call your web services."
+              link="https://github.com/firoze-hossain/thundercall-frontend"
+            />
+          </Col>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={hos}
@@ -35,16 +43,6 @@ function Projects() {
               title="Hospital Management System"
               description="Hospital Management System provides a user-friendly interface, ensuring seamless access and convenience for policyholders across various devices, promoting widespread adoption of Enterprise Solution coverage. Enhances overall customer experience."
               link="https://www.uhlbd.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={geninsurance}
-              isBlog={false}
-              title="General Insurance"
-              description="A comprehensive software solution for general insurance can efficiently manage motor, marine, and fire insurance policies. It offers a user-friendly interface for policyholders to purchase, renew, and make claims seamlessly. The system incorporates robust risk assessment algorithms."
-              link="https://en.wikipedia.org/wiki/General_insurance"
             />
           </Col>
         </Row>
